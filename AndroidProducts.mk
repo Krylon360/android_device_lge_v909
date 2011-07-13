af2e184e94bcfc751ed90f4199973eb5ff649160
krylon360 <krylon360@krylon360-dev.(none)>
2011-07-13 02:13:13 -0600

    Initial Push

---
+++ AndroidProducts.mk
@@ -0,0 +1,2 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/v909.mk
---
+++ v909.mk
@@ -0,0 +1,27 @@
+$(call inherit-product, device/lge/star-common/star.mk)
+
+# Inherit non-open-source blobs.
+$(call inherit-product-if-exists, vendor/lge/v909/v909-vendor.mk)
+
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+	LOCAL_KERNEL := device/lge/v909/kernel
+else
+	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+#DEVICE_PACKAGE_OVERLAYS += device/lge/p999/overlay
+
+# Board-specific init
+PRODUCT_COPY_FILES += \
+    device/lge/v909/init.v909.rc:root/init.rc \
+    #device/lge/v909/vold.fstab:system/etc/vold.fstab \
+  #  device/lge/v909/gps.conf:system/etc/gps.conf \
+    device/lge/v909/init.vsnet:system/bin/init.vsnet
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel \
+    $(LOCAL_PATH)/prebuilt/wireless.ko:system/lib/modules/wireless.ko
+
+PRODUCT_NAME := lge_startablet
+PRODUCT_DEVICE := v909
+PRODUCT_MODEL := LG-V909
